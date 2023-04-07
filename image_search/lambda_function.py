8abff541213866b0a16f1f8f90a2b3e28504c2de
kimonk0299 <64096711+kimonk0299@users.noreply.github.com>
2023-04-07 16:06:52 -0400

    Update lambda_function.py

--- image_search/lambda_function.py
+++ image_search/lambda_function.py
@@ -2,6 +2,7 @@ import json
 import time
 import boto3
 import requests
+#hello
 # Hello
 ES_HOST = 'https://search-photos-gooigaergyjn4jv5pfbr36eapm.us-east-1.es.amazonaws.com'
 REGION = 'us-east-1'
